a0c20d242d8442e8905ae51f73531a2242a53196
yseen <44556800+yseen@users.noreply.github.com>
2022-01-01 22:50:36 +0200

    Update twitterAccess.py
    Updated code for compatibility with Tweepy 4.0. StreamListener has been deprecated, and its functions have been folded into Stream.
    Added filter for English tweets only.
    Changed storage of tweets in csv to include commas. No more removal of commas and white space.
    Adding location information to recorded tweets (in progress) - This will allow for mapping of sentiments for keywords by location in the next step.

--- twitterAccess.py
+++ twitterAccess.py
@@ -7,8 +7,9 @@
 #write sentiment, confidence score, and tweet to out.csv file. 
 #continues running until cancelled. (CTRL+C in shell)
 
+#TODO: record geo coordinates: in progress
 #TODO: Tests with German sentences suggest that Transformers does not work reliably on non-English text. 
-#      Use https://pypi.org/project/langdetect/ to detect English and filter out other tweets.
+#      Use https://pypi.org/project/langdetect/ to detect English and filter out other tweets. DONE! Used languages="en" in stream filter.
 #TODO: run sentimental analysis on original tweets only.    
 #TODO: Remove userhandles before sentiment analysis. 
 #TODO: how to set repeat schedule for pulls?
@@ -31,7 +32,7 @@ access_key      = df.loc[df['keyName'] == "access_key"].iloc[0][1]
 access_secret   = df.loc[df['keyName'] == "access_secret"].iloc[0][1]
 
 # StreamListener class inherits from tweepy.StreamListener and overrides on_status/on_error methods.
-class StreamListener(tweepy.StreamListener):
+class dataWriter(tweepy.Stream):
     def on_status(self, status):
         print(status.id_str)
         # if "retweeted_status" attribute exists, flag this tweet as a retweet.
@@ -42,7 +43,7 @@ class StreamListener(tweepy.StreamListener):
             text = status.extended_tweet["full_text"]
         else:
             text = status.text
-
+        
         # check if this is a quote tweet.
         is_quote = hasattr(status, "quoted_status")
         quoted_text = ""
@@ -58,14 +59,33 @@ class StreamListener(tweepy.StreamListener):
         sentimentLabel = sentiment['label']
         sentimentScore = round(sentiment['score'], 4)
         
-        # remove characters that might cause problems with csv encoding
-        text = ' '.join(text.split())#remove white space
-        text = ' '.join(text.split(',')) # remove commas (breaks CSV) TODO: change code when implementing SQL
-        quoted_text = ' '.join(quoted_text.split())#remove white space
-        quoted_text = ' '.join(quoted_text.split(',')) # remove commas (breaks CSV) TODO: change code when implementing SQL
+        # remove characters that might cause problems with csv encoding         #Removed: use inverted commas to include commas in CSV.
+        #text = ' '.join(text.split())#remove white space
+        #text = ' '.join(text.split(',')) # remove commas (breaks CSV) TODO: change code when implementing SQL
+        #quoted_text = ' '.join(quoted_text.split())#remove white space
+        #quoted_text = ' '.join(quoted_text.split(',')) # remove commas (breaks CSV) TODO: change code when implementing SQL
+        
+        print("The geo is : " + str(status.geo))
+        if (status.geo !=None):
+            print("The geo coordinates are : " + str(status.geo.coordinates))
+        print("The coordinates are : " + str(status.coordinates))
+        if (status.coordinates !=None):
+            print("The coordinates are : " + str(status.coordinates.coordinates))
+        print("The place is : " + str(status.place))
+        if (status.place !=None):
+            print("The place coordinates are: " + str(status.place.bounding_box.type))
+            print("The place coordinates are: " + str(status.place.bounding_box.coordinates))
+            print("The place coordinates are: " + str(status.place.full_name))
+        print("The poster's profile name is : " + status.user.screen_name)
+        if (status.user.location !=None):
+            print("The poster's profile location is : " + status.user.location)
+
 
         with open("out.csv", "a", encoding='utf-8') as f:
-            f.write("%s,%s,%s,%s,%s,%s,%s,%s\n" % (status.created_at,status.user.screen_name,is_retweet,is_quote,text,quoted_text, sentimentLabel, sentimentScore))
+            f.write("%s,%s,%s,%s,\"%s\",\"%s\",%s,%s\n" % (status.created_at,status.user.screen_name,is_retweet,is_quote,text,quoted_text, sentimentLabel, sentimentScore))        #use inverted commas to include commas in CSV.
+                                                    #Note that excel displays in Latin-1 (cp1252) by default.
+                                                    #To correctly display utf-8 (inverted commas, emojis etc.) follow these instructions:
+                                                    #https://answers.microsoft.com/en-us/msoffice/forum/all/how-to-open-utf-8-csv-file-in-excel-without-mis/1eb15700-d235-441e-8b99-db10fafff3c2
 
     def on_error(self, status_code):
         print("Encountered streaming error (", status_code, ")")
@@ -81,15 +101,17 @@ if __name__ == "__main__":
     print(f"pipeline loaded. Time to complete: {elapsed}")
 
     # complete authorization and initialize API endpoint
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
+    #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    #auth.set_access_token(access_key, access_secret)
+    #api = tweepy.API(auth)
 
     # initialize stream
-    streamListener = StreamListener()
-    stream = tweepy.Stream(auth=api.auth, listener=streamListener,tweet_mode='extended')
+    #streamListener = StreamListener()
+    #stream = tweepy.Stream(auth=api.auth, listener=streamListener,tweet_mode='extended')
+    
+    stream = dataWriter(consumer_key, consumer_secret, access_key, access_secret)#stream call as per Tweepy 4.0
     with open("out.csv", "w", encoding='utf-8') as f:
         f.write("date,user,is_retweet,is_quote,text,quoted_text,sentiment,score\n")
     tags = ["food"]
-    stream.filter(track=tags)
+    stream.filter(languages=["en"], track=tags) # ensures that only English tweets are recorded.
 
